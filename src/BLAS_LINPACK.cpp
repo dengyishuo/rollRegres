d9547e9733848b7384791c493b9f5336e4dab648
邓一硕 <dengyishuo@163.com>
2025-05-26 12:07:08 +0800

    Update BLAS_LINPACK.cpp

--- src/BLAS_LINPACK.cpp
+++ src/BLAS_LINPACK.cpp
@@ -4,7 +4,7 @@
 void dtrsm(
     const char *side, const char *uplo, const char *transa, const char *diag,
     const int *m, const int *n, const double *alpha, const double *a,
-    const int *lda, double *b, const int *ldb){
+    const int *lda, double *b, const int *ldb) {
   return F77_CALL(dtrsm)(
       side, uplo, transa, diag, m, n, alpha, a, lda, b, ldb);
 }
